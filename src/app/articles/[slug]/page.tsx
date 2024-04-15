201881a9431bc58e60f37ed0d63289b36bf33527
Tran Van Khuyen <khuyentvhe187268@fpt.edu.vn>
2024-04-15 15:36:34 +0700

    blog page: full

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import ArticleDetail from "@/app/components/articles/ArticleDetail";
 export const metadata: Metadata = {
   title: "Hoshikira | Articles",
-  icons: {
-    icon: "/favicon.png",
-  },
 };
 
 const Post = () => {
